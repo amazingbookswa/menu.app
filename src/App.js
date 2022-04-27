15c8a2ac43e1b7a52999fb8c17dec764d1af3eb8
FRANCIS YELFAARI <94383215+amazingbookswa@users.noreply.github.com>
2022-04-27 08:44:46 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+}
+
+export default App;
